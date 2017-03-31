615dcc5a1061458453afa0bd45f7d7f49c0bce57
Tom Coleman <tom@thesnail.org>
2017-03-31 12:08:53 +1100

    Attempted reproduction of #708

---
+++ src/Component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './Component.scss';
+
+export default function Component() {
+  return <div className="component">Component</div>
+}
---
+++ src/stories/Component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+
+import Component from '../Component';
+
+storiesOf('Component')
+  .add('default', () => <Component />);
